98f12c2c04fab756b5f953ab3dd14c3e41a6fb2c
liyang <673740768@qq.com>
2017-11-26 10:29:43 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -32,6 +33,7 @@ class UsersController extends Controller
         'password' => bcrypt($request->password),
       ]);
 
+      Auth::login($user);
       session()->flash('success', '欢迎，你将在这里开启一段快乐的旅程～');
       return redirect()->route('users.show', [$user]);
     }
