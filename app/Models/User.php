0389061f1dbbbf85524d9b1bd207a8db5e372360
liyang <673740768@qq.com>
2018-04-15 09:39:16 +0000

    Add password resets $ email configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword;
 
 class User extends Authenticatable
 {
@@ -36,6 +37,11 @@ class User extends Authenticatable
         static::creating(function ($user) {
             $user->activation_token = str_random(30);
         });
+    }
+
+    public function  sendPasswordResetNptification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 
     public function gravatar($size = '100')
